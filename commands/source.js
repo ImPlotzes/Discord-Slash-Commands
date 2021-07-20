e4e7a50ad2e2dc3be77f7ea0b8b2ce116d2426ea
Plotzes <plotzes@email.com>
2021-07-20 15:52:28 +0200

    Added a link button to source.js

--- commands/source.js
+++ commands/source.js
@@ -14,11 +14,24 @@ export function handleSource(request, requestBody) {
         embeds: [
             {
                 title: "Source Code",
-                description: "This application is open source. You can check out the GitHub repo with the source code by clicking [HERE](https://github.com/ImPlotzes/Discord-Slash-Commands).",
+                description: "This application is open source. You can check out the GitHub repo with the source code by clicking the button below. Don't be scared to scour through the code and look for bugs.",
                 color: parseInt("76cc00", 16),
                 image: {
                     url: "https://repository-images.githubusercontent.com/387602281/77d1db5e-ad27-4e3e-a463-164cde00c182"
                 }
+            }
+        ],
+        components: [
+            {
+                type: 1,
+                components: [
+                    {
+                        type: 2,
+                        style: 5,
+                        label: "Go to the GitHub repo!",
+                        url: "https://github.com/ImPlotzes/Discord-Slash-Commands"
+                    }
+                ]
             }
         ]
     };
